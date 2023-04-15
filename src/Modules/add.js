f088327cc4de45cc09a9a3c7df2aa1c9dd7f143b
Rachel Brown Gomez <rgbrown21@icloud.com>
2023-04-14 22:50:13 -0400

    Rewrite code to include data attributes & objects

--- src/Modules/add.js
+++ src/Modules/add.js
@@ -1,60 +1,120 @@
-//Add task and add project objs that will be called in the form submit buttons.
-
-// function addedProject(???) - an obj. that will close out the form 
-// and receive input details from that form into the "Projectbody" as an innerHTML text, 
-// element, appendchild, li, ordered list, obj constructor or factory function
 
 // Add project list items variables
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-const addProjectInputTitle = document.querySelector("#title");
-const addProjectInputDetails = document.querySelector("#details");
-const addProjectBtn = document.querySelector(".projBtnSubmit");
-const projListHolder = document.querySelector('.projectListHolder');
-const newProjItem = document.createElement('button');
-
-
-
-addProjectBtn.addEventListener('click', () => {
-
-    const projList = projListHolder.querySelector('.projectList');
-
-    newProjItem.innerHTML = addProjectInputTitle.value;
-    newProjItem.classList = "ease-in-out duration-300 group rounded-lg p-3 bg-white hover:shadow-lg hover: ring-slate-900/5 hover:ring-1";
-    projList.appendChild(newProjItem);
-    addProjectInputTitle.value = '';
-    createDelBtn(newProjItem);
-
+// This is an array OF OBJECTS that asK for an id and a name - this is the list of project titles which will display in the sidebar under 'projects'
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'ray'
+}];
+// Whenever you submit a title on the project form, this will push the new title into the lists array as an object with a unique date id, name and tasks array
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName === '') return
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render()
 })
 
-const del = projList.children;
-
-function createDelBtn(newProjItem) {
-    const erase = document.createElement('button');
-    erase.textContent = 'x';
-    // erase.classList = "text-red-950 ml-2.5"; STYLING FOR X BUTTON NOT WORKING BUT AT LEAST THERE IS NOW A DELETE BUTTON!
-    newProjItem.appendChild(erase);
-    erase.addEventListener('click', function () {
-        this.parentElement.remove();
+// THIS IS AN OBJECT. This will add a unique date id, name, and open tasks array to each new project title added to the lists array above
+function createList(name) {
+    return {
+        id: Date.now().toString(),
+        name: name,
+        tasks: [],
+    }
+}
+// This will append a new child li which the new project title with unique id, name and tasks []
+function render() {
+    // <li class="list-name"
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
+        listElement.classList.add("list-name");
+        listElement.innerText = list.name;
+        listsContainer.appendChild(listElement)
     })
-    return newProjItem;
+}
+// This will clear the project list if there are any old lists from beforehand - may not need this function since there are no project titles written in the HTML page
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
 }
 
-for (let i = 0; i < newProjItem; i++) {
+render()
 
-    createDelBtn(newProjItem[i]);
-}
-
-
+// SO FAR THIS WORKS THE SAME AS THE CODE BELOW. FIGURE OUT A WAY TO MAKE THE FORM DISAPPEAR AFTER HITTING SUBMIT.
+//  NEXT STEP IN THE VIDEO IS SAVING DATA WHEN PAGE RELOADS. VIDEO - 10:35
 
 
+
+
+
+
+
+
+// const addProjectInputTitle = document.querySelector("#title");
+// const addProjectInputDetails = document.querySelector("#details");
+// const addProjectBtn = document.querySelector(".projBtnSubmit");
+// const projListHolder = document.querySelector('.projectListHolder');
+
+// const projList = projListHolder.querySelector('.projectListUl');
+
+// //addProjectBtn may need to become an object with an empty array and data attributes attached
+// addProjectBtn.addEventListener('click', () => {
+//     const newProjItem = document.createElement('li');
+//     const newProjItemBtn = document.createElement('button');
+//     newProjItemBtn.innerHTML = addProjectInputTitle.value;
+//     // newProjItemBtn.classList = 'projectDisplayBtn px-6'; STYLING IS NOT WORKING - TRYING TO STRETCH OUT THE ITEMS TO TAKE UP ALL SPACE AND MAKE X ONLY HAVE SMALL SPACE
+//     newProjItem.classList = "flex grow gap-5 ease-in-out duration-300 rounded-lg p-3 bg-white hover:shadow-lg hover: ring-slate-900/5 hover:ring-1";
+//     projList.appendChild(newProjItem);
+//     newProjItem.appendChild(newProjItemBtn);
+//     addProjectInputTitle.value = '';
+//     createDelBtn(newProjItem);
+
+// })
+
+// const del = projList.children;
+
+// function createDelBtn(newProjItem) {
+//     const erase = document.createElement('button');
+//     erase.textContent = 'x';
+//     // erase.classList = "font-bold"; - maybe use erase.classList.add
+//     newProjItem.appendChild(erase);
+//     erase.addEventListener('click', function () {
+//         this.parentElement.remove();
+//     })
+//     return newProjItem;
+// }
+
+// for (let i = 0; i < newProjItem; i++) {
+
+//     createDelBtn(newProjItem[i]);
+// }
+// CREATING A BUTTON INSIDE OF A BUTTON IS GETTING TOO CRAZY. WATCH KYLES VIDEO FROM 1:00 TO 11:00 TO USE OBJECTS AND SAVE DATA - MIGHT AS WELL LEARN HOW TO USE STORAGE DATA NOW
+
+//CREATE AN EVENT LISTENER FOR NEWPROJITEMBTN - TO DISPLAY IT ON THE MAIN DISPLAY WITH THE ADD TASK BUTTON
+
+// SUCCESS WITH STYLING THE APPENDED CHILD BUTTONS AND ADDING X DELETE BUTTON. NEXT FIX STYLING FOR SAID BUTTON.
+
+// FIXED THE ALIGNMENT OF THE NEW CREATED LIST BUTTONS TO CENTER. 
+// NOW DO THE SAME FOR 'DETAILS'-IF NOT JUST REMOVE 'DETAILS'. NEXT 
+// MAKE EVER7Y NEW BUTTON HAVE A CLICK EVENT TO DISPLA7 ONTO DISPLAY.
 
 // SUCCESS! MADE THE FORM WORK WITH VALUE FOR TITLE AS A NEW BUTTON APPENDED TO "PROJECTLIST"
 // NEXT - DO THE SAME FOR DETAILS AND TRY TO ADD SOME STYLING TO THE APPEDNED BUTTONS
 // THESE APPENDED BUTTONS WILL LATER HAVE THEIR OWN ONCLICK EVENTS.
 
-// FIXED THE ALIGNMENT OF THE NEW CREATED LIST BUTTONS TO CENTER. 
-// NOW DO THE SAME FOR 'DETAILS'-IF NOT JUST REMOVE 'DETAILS'. NEXT 
-// MAKE EVER7Y NEW BUTTON HAVE A CLICK EVENT TO DISPLA7 ONTO DISPLAY.
+
 
 // function addedProject(title, details) { //This is a factory function that will create project objects
 //     return {
--- src/Modules/display.js
+++ src/Modules/display.js
@@ -1,5 +1,6 @@
 //Here will be functions for tab switching for each display item - (Today, This week, Completed, Default(Inbox), Projects)
 function viewProject() {
+    // onClick event - innertext display on Div with an add task button
     // This factory or constructor will create display "objs" based on the button clicked under projects.
     //input/#title/#details/submitbutton.(this.value) = innerTEXT.display
     // call the addTask function button?
@@ -17,10 +18,9 @@ function displayToday() {
     //eventlisteners or append an element that will display a list of tasks - check boxes.
 }
 
-function displayThisWeek() {
-
-}
 
 function displayCompletedTasks() {
 
 }
+
+// WATCH KYLE'S VIDEO STARTING AT 19:30 TO RENDER ITEMS ON TO THE DISPLAY
