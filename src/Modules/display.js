9cd8204a898dc23915ec96e7ab949e36ff483d8d
Rachel Brown Gomez <rgbrown21@icloud.com>
2023-04-27 23:12:59 -0400

    setup HTML for Display function querySelectors

--- src/Modules/display.js
+++ src/Modules/display.js
@@ -1,26 +1,61 @@
-//Here will be functions for tab switching for each display item - (Today, This week, Completed, Default(Inbox), Projects)
-function viewProject() {
-    // onClick event - innertext display on Div with an add task button
-    // This factory or constructor will create display "objs" based on the button clicked under projects.
-    //input/#title/#details/submitbutton.(this.value) = innerTEXT.display
-    // call the addTask function button?
-}
-
-function displaySidebarSelection() {
-    let newDisplay = document.querySelector('.selectedDisplay');
 
 
-}
-
-function displayToday() {
-    //Some kind of onclick event for the "today" button. This will display all 
-    //tasks with a due date of current date.
-    //eventlisteners or append an element that will display a list of tasks - check boxes.
-}
 
 
-function displayCompletedTasks() {
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /Here will be functions for tab switching for each display item - (Today, This week, Completed, Default(Inbox), Projects)
+// function viewProject() {
+//     // onClick event - innertext display on Div with an add task button
+//     // This factory or constructor will create display "objs" based on the button clicked under projects.
+//     //input/#title/#details/submitbutton.(this.value) = innerTEXT.display
+//     // call the addTask function button?
+// }
+
+// function displaySidebarSelection() {
+//     let newDisplay = document.querySelector('.selectedDisplay');
+
+
+// }
+
+// function displayToday() {
+//     //Some kind of onclick event for the "today" button. This will display all 
+//     //tasks with a due date of current date.
+//     //eventlisteners or append an element that will display a list of tasks - check boxes.
+// }
+
+
+// function displayCompletedTasks() {
+
+// }
 
 // WATCH KYLE'S VIDEO STARTING AT 19:30 TO RENDER ITEMS ON TO THE DISPLAY
