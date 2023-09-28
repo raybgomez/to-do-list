05f02c8572ea8bed2f54036ee5e2fa097b017cdc
Rachel Brown Gomez <rgbrown21@icloud.com>
2023-09-27 22:22:16 -0400

    Add css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
     },
 };
