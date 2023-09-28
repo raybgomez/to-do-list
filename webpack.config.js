db4adeb899a0e1207b6ca91aad01c797d54000b3
Rachel Brown Gomez <rgbrown21@icloud.com>
2023-09-27 22:53:47 -0400

    edit test: /\.css$/

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.css$/,
                 use: ["style-loader", "css-loader"],
             },
         ],
